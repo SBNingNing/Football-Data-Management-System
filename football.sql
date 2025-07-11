796decbb502549aef0cd5b0d6d3c569c87a80026
SBNingNing <zhn666@mail.ustc.edu.cn>
2025-07-11 18:47:14 +0800

    sql

--- football.sql
+++ football.sql
@@ -39,7 +39,7 @@ CREATE TABLE `event` (
   CONSTRAINT `fk_event_match` FOREIGN KEY (`MatchID`) REFERENCES `match` (`MatchID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_event_player` FOREIGN KEY (`球员ID`) REFERENCES `player` (`球员ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_event_team` FOREIGN KEY (`球队ID`) REFERENCES `team` (`球队ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +48,7 @@ CREATE TABLE `event` (
 
 LOCK TABLES `event` WRITE;
 /*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES (5,'M1001','进球',1,'AI1',14),(6,'M1001','进球',1,'AI1',15),(7,'M1001','进球',1,'AI1',16),(8,'M1001','红牌',2,'DS1',17),(9,'M1001','乌龙球',2,'DS1',21),(11,'M1001','黄牌',2,'DS2',19),(12,'M1002','进球',1,'AI1',23),(13,'M1002','进球',4,'D1',56),(14,'M1002','进球',4,'D2',45),(15,'M1002','黄牌',1,'AI4',42),(16,'M1002','进球',1,'AI1',14),(17,'M1002','红牌',4,'D1',15),(18,'M1002','乌龙球',4,'D2',16),(19,'M1001','红牌',1,'AI2',15),(20,'M1001','红牌',1,'AI2',15),(21,'M1001','进球',1,'AI4',13),(22,'M1001','红牌',2,'DS2',14),(23,'M1001','乌龙球',2,'DS2',15),(24,'M1001','乌龙球',2,'DS4',16),(25,'M1003','进球',3,'G1',11),(26,'M1003','进球',3,'G2',15);
+INSERT INTO `event` VALUES (5,'M1001','进球',1,'AI1',14),(6,'M1001','进球',1,'AI1',15),(7,'M1001','进球',1,'AI1',16),(8,'M1001','红牌',2,'DS1',17),(9,'M1001','乌龙球',2,'DS1',21),(11,'M1001','黄牌',2,'DS2',19),(12,'M1002','进球',1,'AI1',23),(13,'M1002','进球',4,'D1',56),(14,'M1002','进球',4,'D2',45),(15,'M1002','黄牌',1,'AI4',42),(16,'M1002','进球',1,'AI1',14),(17,'M1002','红牌',4,'D1',15),(18,'M1002','乌龙球',4,'D2',16),(19,'M1001','红牌',1,'AI2',15),(20,'M1001','红牌',1,'AI2',15),(21,'M1001','进球',1,'AI4',13),(22,'M1001','红牌',2,'DS2',14),(23,'M1001','乌龙球',2,'DS2',15),(24,'M1001','乌龙球',2,'DS4',16),(25,'M1003','进球',3,'G1',11),(26,'M1003','进球',3,'G2',15),(27,'M2001','进球',5,'H5',15),(28,'M2001','进球',6,'W1',16),(30,'M2001','红牌',6,'W5',18),(31,'M2001','进球',5,'H5',50),(32,'M2002','进球',5,'H1',4),(33,'M2002','进球',5,'H1',6),(35,'M2002','红牌',7,'S1',12),(36,'M2002','进球',5,'H2',15),(37,'M2002','乌龙球',7,'S2',75),(38,'M2003','红牌',6,'W6',14),(39,'M2003','黄牌',7,'S2',15),(40,'M2003','进球',6,'W6',14),(41,'M3001','进球',8,'he1',45),(42,'M3001','进球',8,'he3',25),(43,'M3001','进球',9,'Y1',42),(44,'M3001','进球',9,'Y3',56);
 /*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -408,7 +408,7 @@ CREATE TABLE `match` (
 
 LOCK TABLES `match` WRITE;
 /*!40000 ALTER TABLE `match` DISABLE KEYS */;
-INSERT INTO `match` VALUES ('M1001','AI VS DS',1,NULL,'2025-07-31 00:00:00','西操',1,2,7,0,'F',NULL),('M1002','AI VS 地空',1,NULL,'2025-07-31 00:00:00','东操',1,4,3,2,'F',NULL),('M1003','AI VS 工院',1,NULL,'2025-07-30 00:00:00','西操',1,3,0,2,'P',NULL);
+INSERT INTO `match` VALUES ('M1001','AI VS DS',1,NULL,'2025-07-31 00:00:00','西操',1,2,7,0,'F',NULL),('M1002','AI VS 地空',1,NULL,'2025-07-31 00:00:00','东操',1,4,3,2,'F',NULL),('M1003','AI VS 工院',1,NULL,'2025-07-30 00:00:00','西操',1,3,0,2,'P',NULL),('M2001','化院 VS 物院 第一轮',2,NULL,'2025-07-31 00:00:00','东操',5,6,2,1,'P',NULL),('M2002','化院 VS 少院 第一轮',2,NULL,'2025-07-31 00:00:00','西操',5,7,4,0,'P',NULL),('M2003','物院 VS 少院 第一轮',2,NULL,'2025-08-07 00:00:00','东操',6,7,1,0,'P',NULL),('M3001','核院 VS 羊村 第一轮',3,NULL,'2025-07-30 00:00:00','西操',8,9,2,2,'P',NULL);
 /*!40000 ALTER TABLE `match` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -543,7 +543,7 @@ CREATE TABLE `player` (
 
 LOCK TABLES `player` WRITE;
 /*!40000 ALTER TABLE `player` DISABLE KEYS */;
-INSERT INTO `player` VALUES ('AI1','AI1',5,0,0),('AI2','AI2',0,2,0),('AI3','AI3',0,0,0),('AI4','AI4',1,0,1),('AI5','AI5',0,0,0),('AI6','AI6',0,0,0),('D1','地1',1,1,0),('D2','地2',1,0,0),('D3','地3',0,0,0),('D4','地4',0,0,0),('D5','地5',0,0,0),('D6','地6',0,0,0),('DS1','DS1',0,1,0),('DS2','DS2',0,1,1),('DS3','DS3',0,0,0),('DS4','DS4',0,0,0),('DS5','DS5',0,0,0),('DS6','DS6',0,0,0),('DS7','DS7',0,0,0),('G1','工院1',1,0,0),('G2','工院2',1,0,0),('G3','工院3',0,0,0),('G4','工院4',0,0,0),('G5','工院5',0,0,0),('G6','工院6',0,0,0);
+INSERT INTO `player` VALUES ('AI1','AI1',5,0,0),('AI2','AI2',0,2,0),('AI3','AI3',0,0,0),('AI4','AI4',1,0,1),('AI5','AI5',0,0,0),('AI6','AI6',0,0,0),('D1','地1',1,1,0),('D2','地2',1,0,0),('D3','地3',0,0,0),('D4','地4',0,0,0),('D5','地5',0,0,0),('D6','地6',0,0,0),('DS1','DS1',0,1,0),('DS2','DS2',0,1,1),('DS3','DS3',0,0,0),('DS4','DS4',0,0,0),('DS5','DS5',0,0,0),('DS6','DS6',0,0,0),('DS7','DS7',0,0,0),('G1','工院1',1,0,0),('G2','工院2',1,0,0),('G3','工院3',0,0,0),('G4','工院4',0,0,0),('G5','工院5',0,0,0),('G6','工院6',0,0,0),('H1','化1',2,0,0),('H2','化2',1,0,0),('H3','化3',0,0,0),('H4','化4',0,0,0),('H5','化5',2,0,0),('H6','化6',0,0,0),('he1','核1',1,0,0),('he2','核2',0,0,0),('he3','核3',1,0,0),('he4','核4',0,0,0),('he5','核5',0,0,0),('he6','核6',0,0,0),('he7','核7',0,0,0),('he8','核8',0,0,0),('S1','少1',0,1,0),('S2','少2',0,0,1),('S3','少3',0,0,0),('S4','少4',0,0,0),('S5','少5',0,0,0),('S6','少6',0,0,0),('W1','物1',1,0,0),('W2','物2',0,0,0),('W3','物3',0,0,0),('W4','物4',0,0,0),('W5','物5',0,1,0),('W6','物6',1,1,0),('Y1','喜羊羊',1,0,0),('Y2','美羊羊',0,0,0),('Y3','沸羊羊',1,0,0),('Y4','懒羊羊',0,0,0),('Y5','暖羊羊',0,0,0),('Y6','慢羊羊',0,0,0),('Y7','灰太狼',0,0,0),('Y8','红太狼',0,0,0);
 /*!40000 ALTER TABLE `player` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -575,7 +575,7 @@ CREATE TABLE `player_team_history` (
   CONSTRAINT `player_team_history_chk_1` CHECK ((`赛事进球数` >= 0)),
   CONSTRAINT `player_team_history_chk_2` CHECK ((`赛事红牌数` >= 0)),
   CONSTRAINT `player_team_history_chk_3` CHECK ((`赛事黄牌数` >= 0))
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -584,7 +584,7 @@ CREATE TABLE `player_team_history` (
 
 LOCK TABLES `player_team_history` WRITE;
 /*!40000 ALTER TABLE `player_team_history` DISABLE KEYS */;
-INSERT INTO `player_team_history` VALUES (1,'AI1',1,1,1,5,0,0,NULL),(2,'AI2',2,1,1,0,2,0,NULL),(3,'AI3',3,1,1,0,0,0,NULL),(4,'AI4',4,1,1,1,0,1,NULL),(5,'AI5',5,1,1,0,0,0,NULL),(6,'AI6',6,1,1,0,0,0,NULL),(13,'G1',1,3,1,1,0,0,NULL),(14,'G2',2,3,1,1,0,0,NULL),(15,'G3',3,3,1,0,0,0,NULL),(16,'G4',4,3,1,0,0,0,NULL),(17,'G5',5,3,1,0,0,0,NULL),(18,'G6',6,3,1,0,0,0,NULL),(19,'D1',1,4,1,1,1,0,NULL),(20,'D2',2,4,1,1,0,0,NULL),(21,'D3',3,4,1,0,0,0,NULL),(22,'D4',4,4,1,0,0,0,NULL),(23,'D5',5,4,1,0,0,0,NULL),(24,'D6',6,4,1,0,0,0,NULL),(25,'DS1',1,2,1,0,0,0,NULL),(26,'DS2',2,2,1,0,0,0,NULL),(27,'DS3',3,2,1,0,0,0,NULL),(28,'DS4',4,2,1,0,0,0,NULL),(29,'DS5',5,2,1,0,0,0,NULL),(30,'DS6',6,2,1,0,0,0,NULL),(31,'DS7',7,2,1,0,0,0,NULL);
+INSERT INTO `player_team_history` VALUES (1,'AI1',1,1,1,5,0,0,NULL),(2,'AI2',2,1,1,0,2,0,NULL),(3,'AI3',3,1,1,0,0,0,NULL),(4,'AI4',4,1,1,1,0,1,NULL),(5,'AI5',5,1,1,0,0,0,NULL),(6,'AI6',6,1,1,0,0,0,NULL),(13,'G1',1,3,1,1,0,0,NULL),(14,'G2',2,3,1,1,0,0,NULL),(15,'G3',3,3,1,0,0,0,NULL),(16,'G4',4,3,1,0,0,0,NULL),(17,'G5',5,3,1,0,0,0,NULL),(18,'G6',6,3,1,0,0,0,NULL),(19,'D1',1,4,1,1,1,0,NULL),(20,'D2',2,4,1,1,0,0,NULL),(21,'D3',3,4,1,0,0,0,NULL),(22,'D4',4,4,1,0,0,0,NULL),(23,'D5',5,4,1,0,0,0,NULL),(24,'D6',6,4,1,0,0,0,NULL),(25,'DS1',1,2,1,0,0,0,NULL),(26,'DS2',2,2,1,0,0,0,NULL),(27,'DS3',3,2,1,0,0,0,NULL),(28,'DS4',4,2,1,0,0,0,NULL),(29,'DS5',5,2,1,0,0,0,NULL),(30,'DS6',6,2,1,0,0,0,NULL),(31,'DS7',7,2,1,0,0,0,NULL),(32,'H1',1,5,2,2,0,0,NULL),(33,'H2',2,5,2,1,0,0,NULL),(34,'H3',3,5,2,0,0,0,NULL),(35,'H4',4,5,2,0,0,0,NULL),(36,'H5',5,5,2,2,0,0,NULL),(37,'H6',6,5,2,0,0,0,NULL),(38,'W1',1,6,2,1,0,0,NULL),(39,'W2',2,6,2,0,0,0,NULL),(40,'W3',3,6,2,0,0,0,NULL),(41,'W4',4,6,2,0,0,0,NULL),(42,'W5',5,6,2,0,1,0,NULL),(43,'W6',6,6,2,1,1,0,NULL),(44,'S1',1,7,2,0,1,0,NULL),(45,'S2',2,7,2,0,0,1,NULL),(46,'S3',3,7,2,0,0,0,NULL),(47,'S4',4,7,2,0,0,0,NULL),(48,'S5',5,7,2,0,0,0,NULL),(49,'S6',6,7,2,0,0,0,NULL),(50,'he1',1,8,3,1,0,0,NULL),(51,'he2',2,8,3,0,0,0,NULL),(52,'he3',3,8,3,1,0,0,NULL),(53,'he4',4,8,3,0,0,0,NULL),(54,'he5',5,8,3,0,0,0,NULL),(55,'he6',6,8,3,0,0,0,NULL),(56,'he7',7,8,3,0,0,0,NULL),(57,'he8',8,8,3,0,0,0,NULL),(58,'Y1',1,9,3,1,0,0,NULL),(59,'Y2',2,9,3,0,0,0,NULL),(60,'Y3',3,9,3,1,0,0,NULL),(61,'Y4',4,9,3,0,0,0,NULL),(62,'Y5',5,9,3,0,0,0,NULL),(63,'Y6',6,9,3,0,0,0,NULL),(64,'Y7',7,9,3,0,0,0,NULL),(65,'Y8',8,9,3,0,0,0,NULL);
 /*!40000 ALTER TABLE `player_team_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -616,7 +616,7 @@ CREATE TABLE `team` (
   CONSTRAINT `team_chk_4` CHECK ((`赛事黄牌数` >= 0)),
   CONSTRAINT `team_chk_5` CHECK ((`赛事积分` >= 0)),
   CONSTRAINT `team_chk_6` CHECK ((`赛事排名` > 0))
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -625,7 +625,7 @@ CREATE TABLE `team` (
 
 LOCK TABLES `team` WRITE;
 /*!40000 ALTER TABLE `team` DISABLE KEYS */;
-INSERT INTO `team` VALUES (1,'AI',NULL,1,6,4,4,2,1,6,1),(2,'DS',NULL,1,0,7,-7,2,1,0,4),(3,'工院',NULL,1,2,0,2,0,0,0,2),(4,'地空',NULL,1,2,3,-1,1,0,0,3);
+INSERT INTO `team` VALUES (1,'AI',NULL,1,6,4,4,2,1,6,1),(2,'DS',NULL,1,0,7,-7,2,1,0,4),(3,'工院',NULL,1,2,0,2,0,0,0,2),(4,'地空',NULL,1,2,3,-1,1,0,0,3),(5,'化院女足',NULL,2,5,1,4,0,0,0,NULL),(6,'物院女足',NULL,2,2,2,0,2,0,0,NULL),(7,'少院女足',NULL,2,0,5,-4,1,1,0,NULL),(8,'核院1号',NULL,3,2,2,2,0,0,0,NULL),(9,'羊村队',NULL,3,2,2,0,0,0,0,NULL);
 /*!40000 ALTER TABLE `team` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -686,7 +686,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin','scrypt:32768:8:1$hashed_password','admin@example.com','admin','2025-07-03 13:10:28',NULL,'A'),(2,'ustc','pbkdf2:sha256:260000$MQZTinQQGBdd1ULB$2a3317be5b8cc9b4fba82686bb6e19eee6f50b3f19fd619b07a4340eb38f4839','2069@qq.com','user','2025-07-03 05:13:05','2025-07-09 11:57:15','A');
+INSERT INTO `user` VALUES (1,'admin','scrypt:32768:8:1$hashed_password','admin@example.com','admin','2025-07-03 13:10:28',NULL,'A'),(2,'ustc','pbkdf2:sha256:260000$MQZTinQQGBdd1ULB$2a3317be5b8cc9b4fba82686bb6e19eee6f50b3f19fd619b07a4340eb38f4839','2069@qq.com','user','2025-07-03 05:13:05','2025-07-11 08:40:44','A');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -707,4 +707,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-09 23:15:38
+-- Dump completed on 2025-07-11 18:45:36
